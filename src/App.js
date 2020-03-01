a74fe77cfa98b7e8b1f4a75f562fdd945dd8e8cf
Alireza Fereydoni <35655080+AlirezaFereydoni@users.noreply.github.com>
2020-03-01 15:08:34 +0330

    Finish Project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import CardList from "./components/card-list/card-list-component";
+import { SearchBox } from "./components/search-box/search-box-component";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -18,9 +24,18 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredState = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monster Roolodex</h1>
+        <SearchBox
+          placeholder="Search Monster"
+          changeHandler={this.handleChange}
+        />
+        <CardList monsters={filteredState} />
       </div>
     );
   }
--- src/components/card/card-component.jsx
+++ src/components/card/card-component.jsx
@@ -6,7 +6,7 @@ const Card = props => {
     <div className="card-container">
       <img
         src={`https://robohash.org/${props.monsters.id}?set=set2&size=180x180`}
-        alt="robot-image"
+        alt="robot"
       />
       <h2>{props.monsters.name}</h2>
       <p>{props.monsters.email}</p>
